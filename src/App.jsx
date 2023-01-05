11a3239c6ca4c029c429eb33a294c6899f93ba8d
James Osunkiyesi <jaydhales@gmail.com>
2023-01-05 21:58:16 +0100

    feat: Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
+const App = () => {
+  return <div className="App">LendSqr</div>;
+};
+
+export default App;
